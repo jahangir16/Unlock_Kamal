3967458ad24e4803235f4971844ecce9792093d1
jahangir16 <kamaljahangir13@gmail.com>
2023-07-13 14:38:59 +0500

    React Router Completed

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,11 @@ import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { create } from "domain";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ContactPage from "../../features/contact/ContactPage";
+import AboutPage from "../../features/about/AboutPage";
+import ProductDetailsPage from "../../features/catalog/ProductDetails";
 
 
 function App() {
@@ -10,16 +15,33 @@ function App() {
     const palettleType = darkMode ? 'dark':'light';
     const theme = createTheme({
         palette:{
-        mode: palettleType
+        mode: palettleType,
+        background:{
+            default:palettleType === 'light' ? '#eaeaea': '#121212'
         }
+       
+        
+        }
+       
     })
+
+  function handleThemeChange(){
+    setDarkMode(!darkMode)
+  }
+
     
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline/> {/* remove the browser Css */}
-            <Header/>
+            <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
             <Container>
-            <Catalog />
+                <Routes>
+                    <Route path='/' Component={HomePage} />
+                    <Route path='/catalog' Component={Catalog}/>
+                    <Route path='/catalog/:id' Component={ProductDetailsPage}/>
+                    <Route path='/contact' Component={ContactPage}/>
+                    <Route path='/about' Component={AboutPage}/>
+               </Routes>
             </Container>
     </ThemeProvider>
   );
