364a0e1ca0e3faa339bdc625f31cb6f713af098c
jahangir16 <kamaljahangir13@gmail.com>
2023-08-16 10:31:35 +0500

    Pagination Part 2 Sort Method

--- WebApplication1/Controllers/ProductsController.cs
+++ WebApplication1/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 using WebApplication1.Entities;
+using WebApplication1.Extensions;
 
 namespace WebApplication1.Controllers
 {
@@ -11,16 +12,29 @@ namespace WebApplication1.Controllers
         private readonly StoreContext _context;
 
         public ProductsController(StoreContext context)
-        {
+        {   
             _context = context;
         }
-        [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        [HttpGet ]
+        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
         {
             // here i made it async so it will wait untill result is found
-            var products = await _context.Products.ToListAsync();
-            return Ok(products);
-        }
+            /*var products = await _context.Products.ToListAsync();
+            return Ok(products);*/
+
+            var querry = _context.Products
+                .Sort(orderBy)
+                .AsQueryable();
+
+            /*querry = orderBy switch
+            {
+                "price" => querry.OrderBy(p => p.Price),
+                "priceDesc" => querry.OrderByDescending(p => p.Price),
+                _ => querry.OrderBy(p => p.Name)
+            };*/
+
+            return await querry.ToListAsync();
+         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
---
+++ WebApplication1/Extensions/ProductExtensions.cs
@@ -0,0 +1,21 @@
+﻿using WebApplication1.Entities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace WebApplication1.Extensions
+{
+    public static class ProductExtensions
+    {
+         public static IQueryable<Product> Sort(this IQueryable<Product> query, string orderBy) 
+        {
+            if(string.IsNullOrWhiteSpace(orderBy)) return query.OrderBy(p=>p.Name);
+            query = orderBy switch
+            {
+                "price" => query.OrderBy(p => p.Price),
+                "priceDesc" => query.OrderByDescending(p => p.Price),
+                _ => query.OrderBy(p => p.Name)
+            };
+            return  query;
+
+        }
+    }
+}
